6cb315022fa4a1a8d867c619b7f7888e63930f86
FourierSignal <sudheer.linx@gmail.com>
2021-01-26 15:24:12 +0530

    initialize_without_gcc_crt0

---
+++ 03_initialize_without_gcc_crt0_v0/hello_world.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+    int main(void) {
+      //printf("Hello world");
+      int a =10;
+      int b = 11;
+      int c;
+      c=a+b;
+      return 0;
+    }
+/*
+void exit(int code)
+{
+    while(1)
+        ;
+}
+*/
