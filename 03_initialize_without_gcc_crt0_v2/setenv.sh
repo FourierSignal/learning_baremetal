7043ad2e23e50254223f7bba28d898cff7fde2b1
FourierSignal <sudheer.linx@gmail.com>
2021-02-06 13:14:26 +0530

    PATH change for toolchain

--- 03_initialize_without_gcc_crt0_v2/setenv.sh
+++ 03_initialize_without_gcc_crt0_v2/setenv.sh
@@ -1,2 +1,7 @@
-export PATH=$PATH:/home/jaguar/Jaguar_workspace/Embedded/RSP3_revB/toolchains/Linaro/baremetal_toolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf/bin
+
+#export PATH=$PATH:/home/jaguar/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/bare-metal-gcc-build/aarch64-elf/bin
+
+export PATH=$PATH:/run/media/jaguar/f1e5af1f-3724-43aa-9a96-e6a60088e8b8/jaguar/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/bare-metal-gcc-build/aarch64-elf/bin
+
+
 echo $PATH
