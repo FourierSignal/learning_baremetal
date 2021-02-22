c8f15fb44a2c24f762a1a68f9c1a398a4ba0d6be
FourierSignal <sudheer.linx@gmail.com>
2021-02-22 07:18:50 +0530

    with crt0

---
+++ 04_initialise_with_gcc_crt0/setenv.sh
@@ -0,0 +1,14 @@
+
+HOME_DIR=/home/jaguar/Mount_dir/jaguar
+#export PATH=$PATH:/home/jaguar/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/bare-metal-gcc-build/aarch64-elf/bin
+
+
+#export PATH=$PATH:$HOME_DIR/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/bare-metal-gcc-build/aarch64-elf/bin
+#export PATH=$PATH:$HOME_DIR/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/bare-metal-gcc-build/gdb/bin
+#export PATH=$PATH:$HOME_DIR/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/build-tools-gcc/toolchain/bin/
+#export PATH=$PATH:$HOME_DIR/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc-arm/gcc-arm-none-eabi-10-2020-q4-major/bin/
+
+export PATH=$PATH:$HOME_DIR/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/bare-metal-gcc-build/aarch64-elf/bin
+export PATH=$PATH:$HOME_DIR/Jaguar_workspace/Embedded/RSP3_revB/toolchains/gcc_from_GNU/bare-metal-gcc-build/gdb/bin
+
+echo $PATH
